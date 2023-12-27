62e498444c809c1c9bb1f78c2a3b61164279a5ee
Patryk Barć <patrykbarc.design@gmail.com>
2023-12-27 14:31:45 +0100

    Basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 function App() {
 	return (
-		<div>
-			<p>Hello world</p>
+		<div className='calculator'>
+			<div className='display'>2137</div>
+			<div className='buttons'>
+				<button>7</button>
+				<button>8</button>
+				<button>9</button>
+				<button>+</button>
+				<button>4</button>
+				<button>5</button>
+				<button>6</button>
+				<button>-</button>
+				<button>1</button>
+				<button>2</button>
+				<button>3</button>
+				<button>*</button>
+				<button>0</button>
+				<button>.</button>
+				<button>C</button>
+				<button>/</button>
+				<button>=</button>
+			</div>
 		</div>
 	)
 }
