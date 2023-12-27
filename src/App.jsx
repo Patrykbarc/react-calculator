16ed3f0c39388b35ae33be83226b5b4defae407f
Patryk Barć <patrykbarc.design@gmail.com>
2023-12-27 17:03:50 +0100

    Buttons mapping + useState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
+import { useState } from 'react'
+
 function App() {
+	const [display, setDisplay] = useState('')
+	console.log(display)
+
+	function handleSetDisplay(e) {
+		console.log(e)
+
+		try {
+			switch (e) {
+				case 'C':
+					setDisplay('')
+					break
+				case '=':
+					setDisplay(eval(display).toFixed(5))
+					break
+
+				default:
+					setDisplay(prevState => prevState + e)
+					break
+			}
+		} catch (error) {
+			setDisplay('error')
+		}
+	}
+
 	return (
 		<div className='calculator'>
-			<div className='display'>2137</div>
+			<div className='display'>{display}</div>
 			<div className='buttons'>
-				<button>7</button>
-				<button>8</button>
-				<button>9</button>
-				<button>+</button>
-				<button>4</button>
-				<button>5</button>
-				<button>6</button>
-				<button>-</button>
-				<button>1</button>
-				<button>2</button>
-				<button>3</button>
-				<button>*</button>
-				<button>0</button>
-				<button>.</button>
-				<button>C</button>
-				<button>/</button>
-				<button>=</button>
+				{[7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', '.', 0, 'C', '/'].map(
+					value => (
+						<button
+							key={value}
+							value={value}
+							onClick={() => handleSetDisplay(value)}>
+							{value}
+						</button>
+					)
+				)}
+				<button
+					disabled={display.length === 0}
+					onClick={() => handleSetDisplay('=')}>
+					=
+				</button>
 			</div>
 		</div>
 	)
